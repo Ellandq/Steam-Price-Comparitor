e9f27f104be843246b0aec857aa8f2536c290103
Mikołaj Wasilewski <waselland@gmail.com>
2023-10-05 09:58:11 +0200

    Initializing repo

--- main.js
+++ main.js
@@ -7,7 +7,10 @@ const path = require('node:path')
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
     win.loadFile('index.html')
@@ -21,4 +24,12 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
 
-//
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+// Hi
